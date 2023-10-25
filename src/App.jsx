fa1a23d262e5f418d0159211a7439843e5098122
GustavoEAybar <aybargustavoe@gmail.com>
2023-10-25 11:59:30 -0300

    modify file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/layout/Header/Header';
-import Punto01 from './components/Punto01';
 import Footer from './components/layout/Footer/Footer';
+
 
 function App() {
 
   return (
     <div>
       <Header/>
-      <Punto01/>
       <Footer/>
     </div>
   )
